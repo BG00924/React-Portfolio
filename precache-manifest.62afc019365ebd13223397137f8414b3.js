9210abd3290697385a0f9241fb53a5d5ffa8f1b5
BG00924 <BG00924@users.noreply.github.com>
2020-10-19 04:08:31 -0500

    Updates

---
+++ precache-manifest.62afc019365ebd13223397137f8414b3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5c0dc9ddcdee3cbc8707f177b06098",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "30368d49f97b58fcdfb2",
+    "url": "/React-Portfolio/static/css/main.6ae8aa4c.chunk.css"
+  },
+  {
+    "revision": "687ffb406f8e03341bc4",
+    "url": "/React-Portfolio/static/js/2.f668e041.chunk.js"
+  },
+  {
+    "revision": "961cf508e89c6b167b43678b586da794",
+    "url": "/React-Portfolio/static/js/2.f668e041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30368d49f97b58fcdfb2",
+    "url": "/React-Portfolio/static/js/main.537f57ad.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/React-Portfolio/static/media/02-run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "75e774f05fce2c7d5ff2266d873fe28e",
+    "url": "/React-Portfolio/static/media/Blake-Gale-Resume.75e774f0.docx"
+  },
+  {
+    "revision": "122c5cb15ec539516c283806568da2e7",
+    "url": "/React-Portfolio/static/media/Budget-Tracker.122c5cb1.JPG"
+  },
+  {
+    "revision": "c59d88b68299e46a6911f8802ac535ce",
+    "url": "/React-Portfolio/static/media/Note-Taker.c59d88b6.JPG"
+  },
+  {
+    "revision": "dfa0f7a0e17455c59d373cfc7d3ac482",
+    "url": "/React-Portfolio/static/media/employee_tracker_example.dfa0f7a0.JPG"
+  },
+  {
+    "revision": "0fbff1be1156120d2748db51b4e76cd1",
+    "url": "/React-Portfolio/static/media/ford (2).0fbff1be.jpg"
+  },
+  {
+    "revision": "5c3fa3a87091ecc16abcf59d00066559",
+    "url": "/React-Portfolio/static/media/gamesearch.5c3fa3a8.JPG"
+  },
+  {
+    "revision": "ea6bcc3b1e18ba8de24542efdb3c166f",
+    "url": "/React-Portfolio/static/media/quiz_intro.ea6bcc3b.JPG"
+  }
+]);
